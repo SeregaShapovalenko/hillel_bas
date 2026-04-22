ab43efae25753dcdf3742752632ce29a0160aa44
boroda <serega.shapovalenko@gmail.com>
2026-04-22 10:32:50 +0300

    first_two_ commit

--- lesson2/home_work2.py
+++ lesson2/home_work2.py
@@ -4,5 +4,5 @@ two_number = numbers // 1000 % 10
 thri_number = numbers // 100 % 10
 foar_number = numbers // 10 % 10
 five_number = numbers % 10
-print((five_number *  10000) + foar_number * 1000 + thri_number * 100 + two_number * 10 + one_number)
+print(five_number *  10000 + foar_number * 1000 + thri_number * 100 + two_number * 10 + one_number)
 
